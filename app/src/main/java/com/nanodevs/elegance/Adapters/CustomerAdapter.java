30f5e5a5383afb9b8be42b32a377c925ac9bdd49
Mudasir10 <mbux1175@gmail.com>
2020-03-03 17:31:43 +0500

    changes made at 7 25

--- app/src/main/java/com/nanodevs/elegance/Adapters/CustomerAdapter.java
+++ app/src/main/java/com/nanodevs/elegance/Adapters/CustomerAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,8 +14,7 @@ import com.nanodevs.elegance.R;
 
 import java.util.List;
 
-public class CustomerAdapter extends RecyclerView.Adapter {
-
+public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.viewHolder> {
 
     private List<Customer> customerList;
     private Context context;
@@ -22,21 +22,27 @@ public class CustomerAdapter extends RecyclerView.Adapter {
     public CustomerAdapter() {
     }
 
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.customeritem, parent,false);
+        return new viewHolder(v);
+    }
+
     public CustomerAdapter(List<Customer> customerList, Context context) {
         this.customerList = customerList;
         this.context = context;
     }
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View v = LayoutInflater.from(context).inflate(R.layout.customeritem, parent);
-        return new viewHolder(v);
-    }
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
 
+       Customer customer=customerList.get(position);
+       holder.CusId.setText("Customer Id : "+customer.getCustomerSerial());
+       holder.CusName.setText("Customer Name : "+customer.getCustomerName());
+       holder.CusPhone.setText("Customer Phone : "+customer.getCustomerContact());
     }
+
 
     @Override
     public int getItemCount() {
@@ -45,8 +51,14 @@ public class CustomerAdapter extends RecyclerView.Adapter {
 
     class viewHolder extends RecyclerView.ViewHolder {
 
+        TextView CusId,CusName,CusPhone;
+
         public viewHolder(@NonNull View itemView) {
             super(itemView);
+            CusId=itemView.findViewById(R.id.customerDispId);
+            CusName=itemView.findViewById(R.id.customerDispName);
+            CusPhone=itemView.findViewById(R.id.customerDispContact);
+
         }
     }
 
--- app/src/main/java/com/nanodevs/elegance/ui/home/HomeFragment.java
+++ app/src/main/java/com/nanodevs/elegance/ui/home/HomeFragment.java
@@ -2,48 +2,114 @@ package com.nanodevs.elegance.ui.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.nanodevs.elegance.Adapters.CustomerAdapter;
+import com.nanodevs.elegance.MainActivity;
 import com.nanodevs.elegance.Pojo.Customer;
 import com.nanodevs.elegance.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
+    private DatabaseReference mDataBaseRef;
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
 
-    @Override
-    public void onStart() {
-        super.onStart();
+    List<Customer> mCustomerList;
 
-
-    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        this.setHasOptionsMenu(true);
         init(root);
 
         return root;
     }
 
     private void init(View root) {
-
+        mDataBaseRef= FirebaseDatabase.getInstance().getReference("Customer");
         recyclerView = root.findViewById(R.id.recyclerViewCustomers);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        mAdapter = new CustomerAdapter();
+
+        mCustomerList=new ArrayList<>();
+
+        ReadAllCustomers();
+
+        CustomerAdapter adapter = new CustomerAdapter(mCustomerList,getContext());
+        recyclerView.setAdapter(adapter);
+
+    }
+
+
+    private void ReadAllCustomers(){
+
+        mCustomerList.clear();
+        mDataBaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
+                 Customer customer=snapshot.getValue(Customer.class);
+                 mCustomerList.add(customer);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        // Inflate the menu; this adds items to the action bar if it is present.
+         inflater.inflate(R.menu.main, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Toast.makeText(getContext(), "Searching Items", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
 
     }
 }
